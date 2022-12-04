75f559c2b449352bcc79051e81adeedf2c2eecc0
Khanh Chung <khanh.d.chung@gmail.com>
2022-12-04 12:43:20 -0800

    Add a function to play a single round and a function to check winner

--- main.js
+++ main.js
@@ -9,3 +9,41 @@ function getComputerChoice() {
       return 'scissors';
   }
 }
+
+// Value based: Computer === -1, Draw === 0, Player === 1
+function checkWinner(playerSelection, computerSelection) {
+  // Handling case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  // No one won
+  if (playerSelection === computerSelection) {
+    return 0;
+  }
+
+  // Computer won
+  if ((playerSelection === 'rock'     && computerSelection === 'paper')    ||
+      (playerSelection === 'paper'    && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'rock')) {
+    return -1;
+  }
+
+  // Player won
+  return 1;
+}
+
+function playRound(playerSelection, computerSelection) {
+  const winner = checkWinner(playerSelection, computerSelection);
+  if (winner === 0) {
+    return 'Its draw';
+  }
+
+  return winner === 1
+    ? `You Won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    : `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(`Player: ${playerSelection} <==> ${computerSelection} :Computer`)
+console.log(playRound(playerSelection, computerSelection));
